f6fcf76133a497b7285cf15e4404f8ed0e3eb5f2
Mystic Kitty <djabranton@gmail.com>
2020-11-10 12:14:33 -0800

    Created generate.js in src file to write and copy html and style.css in to dist folder using promises in generate-site.js. Cleaned up some code in app.js.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name, github);
